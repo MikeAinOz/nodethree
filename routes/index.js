97050dda7ade3874620025e8bab313580f1580d1
MikeAinOz <mike@mikeallen.com.au>
2017-05-13 09:16:56 +1000

    working with request

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Node Three' });
+  var message = "Not Received";
+  request('http://10.1.1.10/api', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+
+	var params = body.split('|');
+	message = params[0] + ' C and ' + params[1] + '% humidity';
+	res.render('index', { title: 'Node Three' , message: message });  
+  	}    
+  	else {
+    	console.log("Error "+response.statusCode)
+  	}
+	});
 });
 
 module.exports = router;
